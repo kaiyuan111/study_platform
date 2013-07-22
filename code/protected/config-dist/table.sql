70ccd89eb3b4be7c6c08ceffe80c5223f9a545fd
hucong <hucong@360.cn>
2013-07-22 21:32:47 +0800

    table sql

--- code/protected/config-dist/table.sql
+++ code/protected/config-dist/table.sql
@@ -4,10 +4,13 @@ CREATE TABLE `m-action` (
   `aname` varchar(20) NOT NULL DEFAULT '',
   `route` varchar(100) NOT NULL DEFAULT '',
   `is_menu` tinyint(1) NOT NULL DEFAULT '0',
+  `logo` varchar(100) NOT NULL DEFAULT '',
+  `logo_click` varchar(100) NOT NULL DEFAULT '',
   `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`aid`),
   UNIQUE KEY `aname` (`aname`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8
+
 
 
 CREATE TABLE `m-role` (
