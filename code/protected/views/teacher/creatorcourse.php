53602187e2091730dd1e09254159d7d8fec339c6
apple <shanxiwgp@163.com>
2013-08-03 14:55:02 +0800

    first

---
+++ code/protected/views/teacher/creatorcourse.php
@@ -0,0 +1,42 @@
+<div class="Add_Class_content">
+	<div class="top"><img src="images/cont_home.png" align="absmiddle" class="home" /><span>课程管理</span>
+	<!--  <div class="right_top_sous">
+	<form action="#">
+    	<ul>
+        	<li class="li"><span class="k">
+       	  	<input name="name" type="text" size="21"/>
+        	<a href="#"><img src="images/im11.jpg" width="71" height="31" border="0" class="right_top_anniu" /></a></span></li>
+        </ul>
+    </form> 
+	</div>-->
+	</div>
+ 	<div class="cont">
+     	<div class="tittle_a">创建课程</div>
+         <div class="form">
+         	<form action="/teacher/savecourse" method="post">
+             	<ul>
+                 	<li class="li"><span class="h">课程名称&nbsp;：<font color="#cb0000">*&nbsp;&nbsp;</font></span><span class="k">
+                 	<input type="text" name="name" class="input" size="29"/></span>
+                 	</li>
+                 	<li class="o_none"><span class="h">所属科目&nbsp;：<font color="#cb0000">*&nbsp;&nbsp;</font></span>
+                 		<span class="k_a">
+                             <select name="classid" style="width:150px">
+                                 <option value="0">选择你想要的科目&nbsp;</option>
+                                 <?php foreach ($courseClass as $key => $value){?>
+                                 <option value="<?php echo $value['id'];?>"><?php echo $value['name'];?>&nbsp;</option>
+                                 <?php }?>
+                             </select>
+                         </span>
+           				<div class="right_tianjia">
+                        <div align="left"><a href="#">+ 添加科目</a></div>
+                    	</div>
+                    </li>
+                 	<li class="li"><span class="h">课程简介&nbsp;：<font color="#cb0000">*&nbsp;&nbsp;</font></span><span >
+                 	<textarea name="desc" cols="49" rows="10" ></textarea>
+                 	</span></li>
+                 </ul>
+             <div class="submit"><input type="submit" value="&nbsp;&nbsp;确&nbsp;&nbsp;定&nbsp;&nbsp;" /></div>
+             </form>         	
+         </div>
+     </div>
+</div>
