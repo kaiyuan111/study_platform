8f8dafa97122af199d3d0a6f2b6f685ea8c7e705
hucong <hucong@360.cn>
2013-08-08 20:59:05 +0800

    ss

---
+++ code/protected/models/CourseClass.php
@@ -0,0 +1,16 @@
+<?php
+
+class CourseClass extends CActiveRecord
+{
+    public static function model($className=__CLASS__)
+    {
+        return parent::model($className);
+    }
+
+    public function tableName()
+    {
+        return '`m-courseclass`';
+    }
+
+    
+}
