a02d711401e41a992c632526f0d0bf909f17ac22
apple <shanxiwgp@163.com>
2013-08-08 20:42:01 +0800

    fifth

--- code/protected/models/group.php
+++ code/protected/models/group.php
@@ -11,25 +11,4 @@ class Group extends CActiveRecord
     {
         return '`m-group`';
     }
-
-    //创建小组
-    public function updateGroup($groupName, $creator, $courseId, $desc, $joinType, $icon = '', $groupId = 0)
-    {
-    	/*if (empty($groupId))   //新建
-    	{
-    		//$sql = 
-    	}
-    	else  //更新
-    	{
-    		
-    	}
-        $conn = Yii::app()->db;
-        $command = $conn->createCommand($sql);
-        
-        $rows = $command->queryAll();
-        return $rows;*/
-    	
-    }
-    
-    
 }
